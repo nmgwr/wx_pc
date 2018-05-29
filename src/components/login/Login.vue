08b03b003903b96089646341c6752087edcf8db1
wenrui3 <wenrui3@asiainfo.com>
2018-05-29 22:54:57 +0800

    调整项目

---
+++ src/components/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: '我是登陆页模板'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
