442c2ea85d8515f0b0d4f434994bdabf21296dd0
wenrui3 <wenrui3@asiainfo.com>
2018-06-04 15:36:33 +0800

    支持sass、登陆页画完了

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     <div class="bg">
-      <el-input
-        placeholder="请输入用户名"
-        v-model="loginName"
-        clearable>
-      </el-input>
+      <el-form class="loginForm" :model="loginForm">
+        <el-form-item style="width: 35vw;margin-top: 15vh;margin-left: 7vw;">
+          <el-row>
+            <el-col :span="12" style="font-size: 2.5vh;width: 10vw;font-weight: 700;color: #fff;text-align:right">用户名：</el-col>
+            <el-col :span="12">
+              <el-input v-model="loginForm.loginName" placeholder="请输入用户名" clearable></el-input>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item style="width: 35vw;margin-top: 1vh;margin-left: 7vw;">
+          <el-row>
+            <el-col :span="12" style="font-size: 2.5vh;width: 10vw;font-weight: 700;color: #fff;text-align:right">密&nbsp;&nbsp;&nbsp;码：</el-col>
+            <el-col :span="12">
+              <el-input v-model="loginForm.passwd" placeholder="请输入密码" clearable></el-input>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item style="text-align:right">
+          <el-button type="" @click="login()" round style="width:17.5vw;color:red"  :disabled="loginForm.loginName == '' || loginForm.passwd == ''">登陆</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -15,12 +31,20 @@ export default {
   name: 'Login',
   data () {
     return {
-      loginName: ''
+      loginForm: {
+        loginName: '',
+        passwd: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.loginForm)
     }
   }
 }
 </script>
-<style scoped>
+<style scoped lang="scss" type="text/css">
 .bg{
   background:url(../../assets/img/login-bg.png);
   width: 100vw;
@@ -28,5 +52,12 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   border: none;
+}
+.loginForm{
+  width: 34.5vw;
+  height: 50vh;
+  position: absolute;
+  margin-top: 20vh;
+  margin-left: 25vw;
 }
 </style>
